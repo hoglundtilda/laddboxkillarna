9923a6bebec50a5612343c6520547bd9b1adac12
jensengbg-fredrik-berntsson <fredrikberntsson@hotmail.com>
2021-05-11 13:45:37 +0200

    upd

---
+++ components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="header__section">
+    <section class="text">
+      <h1>Din helhetslösning</h1>
+      <div>
+        <p class="check">&#x2714; Köp din laddbox</p>
+        <p class="check">&#x2714; Installerat & klart</p>
+        <p class="check">&#x2714; Inga mellanhänder</p>
+        <p class="check">&#x2714; Till Fast Pris</p>
+      </div>
+      <p class="description">
+        Vår helhetslösning går ut på att göra det så enkelt som möjligt för dig
+        som kund. Vi sköter allt inkl. installation. Inga mellanhänder som drar
+        ut på tiden eller trissar upp priser. Det enda du behöver göra är att
+        lägga en beställning. Vi garanterar återkoppling inom 24 timmar
+      </p>
+    </section>
+    <button>Produkter</button>
+    <button>Beställ här</button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+#header__section {
+  color: $white;
+  height: 100vh;
+  background: linear-gradient(
+      180deg,
+      rgba(18, 18, 18, 0) 0%,
+      rgba(18, 18, 18, 0.87) 78.65%,
+      #121212 100%
+    ),
+    url('@/assets/images/background_laddboxkillarna_1.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  padding: 10%;
+
+  .check {
+    padding: 0.5rem 0;
+  }
+  .description {
+    width: 50%;
+  }
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -3,35 +3,27 @@ export default {
   head: {
     title: 'laddboxkillarna',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'vant/lib/index.css',
-    '~assets/css/main'
-  ],
+  css: ['vant/lib/index.css', '~assets/css/main'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '@/plugins/vant'
-  ],
+  plugins: ['@/plugins/vant'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -44,14 +36,12 @@ export default {
   axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
+  build: {},
 
   styleResources: {
     scss: [
-        '~/assets/css/*.scss',
-        //'~/assets/css/mixins.scss',
-
-    ]
-}
+      '~/assets/css/*.scss',
+      //'~/assets/css/mixins.scss',
+    ],
+  },
 }
