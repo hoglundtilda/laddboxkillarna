54831d0c6e1d9cccc6e70ae5ea42a921a162c9f8
matilda.hoglund <hoglund.matilda@gmail.com>
2021-05-17 15:32:46 +0200

    Update actions.js

--- store/actions.js
+++ store/actions.js
@@ -2,7 +2,7 @@ export default {
     async contactEmail({ commit }, email) {
       console.log(email)
       const response = await this.$axios
-        .$post(`/email/contact`, {email})
+        .$post(`/api/email/contact`, {email})
         .then((response) => response.data)
         .catch((error) => {
           console.log(error)
